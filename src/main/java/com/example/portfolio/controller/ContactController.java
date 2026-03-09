4ec58fcb1fe09c4f982bcdbbc08948aa68137d59
xxui <aiu@>
2026-03-09 16:09:08 +0900

    feat:受信メールの内容がさらに詳細に届くように変更

--- src/main/java/com/example/portfolio/controller/ContactController.java
+++ src/main/java/com/example/portfolio/controller/ContactController.java
@@ -25,13 +25,23 @@ public class ContactController {
     public String send(@ModelAttribute ContactForm contactForm) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo("自分のメールアドレス"); // 送信先
-        message.setSubject("ポートフォリオからのお問い合わせ: " + contactForm.getName());
-        message.setText("目的: " + contactForm.getPurpose() + "\n" +
-                "内容: " + contactForm.getMessage());
+        message.setSubject("【ポートフォリオより】: " + contactForm.getName() + "様からのお問い合わせ");
 
-        mailSender.send(message); // ここでメール送信！
+        //全項目を連結して本文を作成
+        StringBuilder body = new StringBuilder();
+        body.append("お問い合わせ目的: ").append(contactForm.getPurpose()).append("\n");
+        body.append("お名前: ").append(contactForm.getName()).append("\n");
+        body.append("企業・団体名: ").append(contactForm.getCompany()).append("\n");
+        body.append("役職: ").append(contactForm.getPosition()).append("\n");
+        body.append("URL: ").append(contactForm.getUrl()).append("\n");
+        body.append("メールアドレス: ").append(contactForm.getEmail()).append("\n");
+        body.append("----------------------------\n");
+        body.append("お問い合わせ内容:\n").append(contactForm.getMessage());
 
-        //送信完了ページにとばす
+        message.setText(body.toString());
+
+        mailSender.send(message); // ここでメール送信
+
         return "redirect:/contact/thanks";
     }
 

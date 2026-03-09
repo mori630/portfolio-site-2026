0990ce0248319d8eb073cfeee3ca83bba7c5bd10
xxui <aiu@>
2026-03-09 15:04:44 +0900

    Add pom.xml to Lombok

---
+++ src/main/java/com/example/portfolio/form/ContactForm.java
@@ -0,0 +1,13 @@
+package com.example.portfolio.form;
+
+import lombok.Data;
+@Data
+public class ContactForm {
+    private String purpose;
+    private String name;
+    private String email;
+    private String company;
+    private String position;
+    private String url;
+    private String message;
+}

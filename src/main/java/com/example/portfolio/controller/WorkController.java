1ed3262a0df983a239620f8a8e9da66310f120c4
xxui <aiu@>
2026-03-09 10:14:40 +0900

    Add index and detail pages

--- src/main/java/com/example/portfolio/controller/WorkController.java
+++ src/main/java/com/example/portfolio/controller/WorkController.java
@@ -1,9 +1,11 @@
 package com.example.portfolio.controller;
 
+import com.example.portfolio.model.Work;
 import com.example.portfolio.repository.WorkRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class WorkController {
@@ -18,5 +20,12 @@ public class WorkController {
     public String index(Model model) {
         model .addAttribute("works", workRepository.findAll());
         return "index";
+    }
+
+    @GetMapping("/work/{id}")
+    public String detail(@PathVariable("id") Long id, Model model) {
+        //IDをもとに作品を取得。なければエラー（トップへ）
+        Work work = workRepository.findById(id).orElseThrow(() -> new RuntimeException("Work not found"));
+        return "detail";
     }
 }
